296860296aa37295df998dec14d1a9d159f66577
shibe23 <shibe23.info@gmail.com>
2021-06-01 23:39:28 +0900

    Update setup.sh

--- node/setup.sh
+++ node/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm init -y
-npm i -D eslint eslint-config-prettier
+npm i -D eslint eslint-config-standard eslint-config-prettier
 curl -OL "https://raw.githubusercontent.com/shibe23/setup-scripts/main/node/.eslintrc.json"
