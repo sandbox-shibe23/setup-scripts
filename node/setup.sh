1044b719e36b8c03c2ae538d05a1c5f59c414d86
shibe23 <shibe23.info@gmail.com>
2021-06-02 00:47:32 +0900

    Update setup.sh

--- node/setup.sh
+++ node/setup.sh
@@ -3,7 +3,7 @@ npm init -y
 npm i -D eslint eslint-config-prettier
 curl -OL "https://raw.githubusercontent.com/shibe23/setup-scripts/main/node/.eslintrc.json"
 mkdir src
-touch main.js
+touch src/main.js
 
 ## package.jsonを編集
 mv package.json temp.json
