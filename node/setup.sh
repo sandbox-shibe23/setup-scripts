3a0a5737af79b0a168fcee7df990111c43065cf3
shibe23 <shibe23.info@gmail.com>
2021-06-02 00:45:31 +0900

    Update setup.sh

--- node/setup.sh
+++ node/setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 npm init -y
-npm i -D eslint eslint-config-standard eslint-config-prettier
+npm i -D eslint eslint-config-prettier
 curl -OL "https://raw.githubusercontent.com/shibe23/setup-scripts/main/node/.eslintrc.json"
+mkdir src
+touch main.js
+
+## package.jsonを編集
+mv package.json temp.json
+cat temp.json \
+| jq 'del(.scripts.test)' \
+| jq '.scripts |= .+ {"format": "prettier --write src/**/*.js"}' \
+| jq '.scripts |= .+ {"lint": "eslint --fix src/**/*.js"}' \
+| jq '.scripts |= .+ {"fix": "npm run format && npm run lint"}' \
+> package.json
+rm -f temp.json
